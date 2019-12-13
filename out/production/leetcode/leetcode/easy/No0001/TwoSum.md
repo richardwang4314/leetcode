b31e2698cacd1e9e00bc048efc16249b0c9bb41e
Arthur <keplertw018@gmail.com>
2019-12-13 15:36:00 +0800

    add Palindrome

---
+++ out/production/leetcode/leetcode/easy/No0001/TwoSum.md
@@ -0,0 +1,22 @@
+#1. Two Sum
+`Easy`
+
+Given an array of integers, return **indices** of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have **exactly** one solution, and you may not use the same element twice.
+
+##Example:
+
+```
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+```
+
+##心得
+my answer `twoSum` O(n^2)
+
+Runtime: 30 ms, faster than 12.71% of Java online submissions for Two Sum.
+Memory Usage: 37.4 MB, less than 98.95% of Java online submissions for Two Sum.
+
+有想過用map但沒想到可以這樣做
---
+++ src/test/Palindrome.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by zihao on 2019/12/12.
+ * [演算法] Is Palindrome：判斷順寫逆寫是不是一樣
+ */
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Madam, I'm Adam"));
+    }
+
+    private static boolean isPalindrome(String str) {
+        String lowStr = str.toLowerCase();
+        Pattern pattern = Pattern.compile("[a-z]");
+        StringBuilder sb = new StringBuilder();
+        for (String s : lowStr.split("")) {
+            if (pattern.matcher(s).matches())
+                sb.append(s);
+        }
+        String s1 = sb.toString();
+        String s2 = sb.reverse().toString();
+        return s1.equals(s2);
+    }
+}
